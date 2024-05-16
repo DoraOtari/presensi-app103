2a7bb07ef877dca173e8f2263b31d2fed9867952
DoraOtari <doraotari@gmail.com>
2024-05-16 10:50:49 +0700

    fitur nik otomatis

--- app/Http/Livewire/NikOtomatis.php
+++ app/Http/Livewire/NikOtomatis.php
@@ -3,12 +3,23 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 
 class NikOtomatis extends Component
 {
+    public $tgl;
+    public $userId;
+
+    public function buatNik() {
+        return Str::replace('-','',$this->tgl).$this->userId;
+    }
+
     public function render()
     {
-        return view('livewire.nik-otomatis',['list_users' => DB::table('users')->get()]);
+        return view('livewire.nik-otomatis',[
+            'list_users' => DB::table('users')->get(),
+            'nik' => $this->buatNik(),
+        ]);
     }
 }
--- resources/views/livewire/nik-otomatis.blade.php
+++ resources/views/livewire/nik-otomatis.blade.php
@@ -1,14 +1,14 @@
 <div>
     <div class="mb-3">
         <label class="form-label"><span class="badge bg-dark">NIK</span></label>
-        <input type="text" class="form-control-plaintext" name="nik" readonly />
+        <input value="{{ $nik }}" type="text" class="form-control-plaintext" name="nik" readonly />
     </div>
     <div class="row">
         <div class="col">
             <div class="mb-3">
                 <label class="form-label">Email</label>
-                <select class="form-select" name="user_id">
-                    <option selected disabled>Select one</option>
+                <select wire:model="userId" class="form-select" name="user_id">
+                    <option value="null" selected disabled>Select one</option>
                     @foreach ($list_users as $user)
                     <option value="{{ $user->id }}">{{ $user->email }}</option>
                     @endforeach
@@ -18,7 +18,7 @@
         <div class="col">
             <div class="mb-3">
                 <label class="form-label">Tanggal Lahir</label>
-                <input type="date" class="form-control" name="tanggal_lahir" />
+                <input wire:model="tgl" type="date" class="form-control" name="tanggal_lahir" />
             </div>
         </div>
     </div>
